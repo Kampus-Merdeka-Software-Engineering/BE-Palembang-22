6e62a84a98fc4bb83be32635620cf11c6d52cc51
ranggad <rangga.drendra46@gmail.com>
2023-10-05 19:32:40 +0700

    Tambah dikit

--- utils/startSequelize.js
+++ utils/startSequelize.js
@@ -8,7 +8,7 @@ import { Sequelize } from "sequelize"
 export const startSeqelize = async (sequelize) =>{
   try {
     await sequelize.authenticate();
-    await sequelize.sync({alter: true})
+    // await sequelize.sync({alter: true});
     console.log("Connection to databases");
     console.log(`Models available in sequelize: ${Object.keys(sequelize.models)}`);
     
